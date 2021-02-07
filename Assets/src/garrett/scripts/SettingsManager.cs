63cdd9587bb5eb83de1d11c47eef63a580915c23
Garrett <deepg2w@gmail.com>
2021-02-06 19:54:40 -0800

    Added Menus
    Added menu functionality and layout

---
+++ Assets/src/garrett/scripts/SettingsManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsManager : MonoBehaviour
+{
+
+	public AudioMixer audio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Toggle Windowed Setting
+    public void ToggleWindowed(bool arg)
+    {
+
+    }
+
+    // change volume with slider
+    // TODO: expose the volume variable in audio mixer
+    public void SetVolume(float vol)
+    {
+    	Debug.Log("setting volume to " + vol);
+    	// audio.SetFloat("volume", volume); // TODO: attach this
+    }
+
+    // set the quality level of the game
+    // attach to drop down in settings menu
+    public void SetQuality(int index)
+    {
+    	QualitySettings.SetQualityLevel(index);
+    }
+}
