a7173cd0e128b3edcd817f4ec9d9a4b7ee68e4e0
synthchris666 <63672369+lotkey@users.noreply.github.com>
2021-03-24 15:36:37 -0700

    Added comments

--- Assets/tst/chris/PlayModeTests/SoundManagerTests.cs
+++ Assets/tst/chris/PlayModeTests/SoundManagerTests.cs
@@ -6,19 +6,31 @@ using UnityEngine.TestTools;
 public class SoundManagerTests
 {
     [UnityTest]
+    // Boundary test for the SoundManager class
+    // Attempting to play a sound by name
+    //   where there is no sound with the specified name
     public IEnumerator PlayNonexistentSoundTest()
     {
+        // Instantiate a new GameObject and add a SoundManager to it
         GameObject gameObject = new GameObject();
         SoundManager soundManager = gameObject.AddComponent<SoundManager>();
+        // The SoundManager has no sounds added to it, so any string should fail
+        // In the case that no sound exists, the Play() function will return false
         Assert.AreEqual(expected: false, actual: soundManager.Play("This should return false because there is no sound with this name"));
         yield return null;
     }
 
     [UnityTest]
+    // Boundary test for the MusicSoundManager class
+    // Attempting to play a musicSound by name
+    //   where there is no musicSound with the specified name
     public IEnumerator PlayNonexistentMusicSoundTest()
     {
+        // Instantiate a new GameObject and add a MusicSoundManager to it
         GameObject gameObject = new GameObject();
         MusicSoundManager musicSoundManager = gameObject.AddComponent<MusicSoundManager>();
+        // The MusicSoundManager has no sounds added to it, so any string should fail
+        // In the case that no sound exists, the Play() function will return false
         Assert.AreEqual(expected: false, actual: musicSoundManager.Play("This should return false because there is no sound with this name"));
         yield return null;
     }
