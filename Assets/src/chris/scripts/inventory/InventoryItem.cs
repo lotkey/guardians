ded8f3c7e696761d687a20a785af2afcb279d366
synthchris666 <63672369+lotkey@users.noreply.github.com>
2021-03-24 16:15:27 -0700

    Commented out WIP buggy inventory stuff

---
+++ Assets/src/chris/scripts/inventory/InventoryItem.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+public class InventoryItem
+{
+    protected string name = "New Item";
+    protected Sprite icon = null;
+    protected string[] attributes;
+    protected float[] attributeValues;
+
+    public InventoryItem(string name, Sprite icon, string[] attributes, float[] attributeValues)
+    {
+        this.name = name;
+        this.icon = icon;
+        this.attributes = attributes;
+        this.attributeValues = attributeValues;
+    }
+
+    public override string ToString()
+    {
+        string description = name;
+        for (int i = 0; i < attributes.Length; i++)
+        {
+            description += $"\n{attributes[i]}";
+            if (attributeValues.Length < i)
+            {
+                description += $": {attributeValues[i]}";
+            }
+        }
+        return description;
+    }
+
+    public Sprite GetIcon()
+    {
+        return icon;
+    }
+}
--- Assets/src/chris/scripts/inventory/Item.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item")]
-public class Item : ScriptableObject
-{
-    new public string name = "New Item";
-    public Sprite icon = null;
-}
