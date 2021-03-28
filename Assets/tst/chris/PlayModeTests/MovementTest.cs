975daa98e0b58558ec76a081930dd5b116db9ab5
synthchris666 <63672369+lotkey@users.noreply.github.com>
2021-03-28 14:11:55 -0700

    Update MovementTest.cs

--- Assets/tst/chris/PlayModeTests/MovementTest.cs
+++ Assets/tst/chris/PlayModeTests/MovementTest.cs
@@ -25,7 +25,51 @@ public class MovementTest
         for (int i = 10; equal; i += i)
         {
             // Move for 100 times
-            for (int j = 0; j < i; j++)
+            for (int j = (int)(i / 2); j < i; j++)
+            {
+                // The amount to move in the y direction will be -1, 0, or 1. Likewise for x
+                int y = (int)Random.Range(-1, 2);
+                int x = (int)Random.Range(-1, 2);
+                // Instead of passing in (x, y, Time.fixedDeltaTime) I pass in (x, y, 1) for easy math
+                entity.movement.Move(x, y, 1);
+                // Since the "deltaTime" is 1 then the position should be updated with x * 1 and y * 1, or x and y
+                expectedPosition.x += x;
+                expectedPosition.y += y;
+            }
+            // Wait a FixedUpdate() to check
+            yield return new WaitForSeconds(Time.fixedDeltaTime);
+
+            equal = expectedPosition.Equals(gameObject.transform.position);
+            if (!equal)
+            {
+                Debug.Log($"Test failed at {i} movements.");
+            }
+        }
+        Assert.IsTrue(false);
+    }
+
+    [UnityTest]
+    // Stress test for the EntityMovement class
+    // Primarily used by the PlayerController
+    // Rounded
+    public IEnumerator MovementStressTestRounded()
+    {
+        // Instatiate a GameObject and add an Entity to it
+        GameObject gameObject = new GameObject();
+        Entity entity = gameObject.AddComponent<Entity>();
+        // Initialize some vectors to (0, 0, 0)
+        // This might be redundant or unneccessary
+        gameObject.transform.position = new Vector2(0, 0);
+        Vector3 expectedPosition = new Vector3(0, 0, 0);
+        entity.body.position = new Vector2(0, 0);
+        // Set the Entity's speed to 1 for easier math
+        entity.movement.speed = 1;
+        bool equal = true;
+
+        for (int i = 10; equal; i += i)
+        {
+            // Move for 100 times
+            for (int j = (int)(i / 2); j < i; j++)
             {
                 // The amount to move in the y direction will be -1, 0, or 1. Likewise for x
                 int y = (int)Random.Range(-1, 2);
@@ -55,7 +99,6 @@ public class MovementTest
                 Debug.Log($"Test failed at {i} movements.");
             }
         }
-        // Compare the arrays
         Assert.IsTrue(false);
     }
 }
