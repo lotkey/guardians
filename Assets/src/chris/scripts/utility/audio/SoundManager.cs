39368c6a4da0fe10376cba3d0d7f231fd3c61f00
synthchris666 <63672369+lotkey@users.noreply.github.com>
2021-03-28 14:05:59 -0700

    Fixed Movement stress test

--- Assets/src/chris/scripts/utility/audio/SoundManager.cs
+++ Assets/src/chris/scripts/utility/audio/SoundManager.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
-    private Sound[] sounds = null;
+    public Sound[] sounds = null;
     private static SoundManager instance;
     [Range(0.0f, 1.0f)]
-    public float volumeOfAllSounds = 1.0f; // Just a temporary field to test controlling volume
+    protected float volumeOfAllSounds = 1.0f; // Just a temporary field to test controlling volume
     private List<Sound> currentSounds = null;
+
+    public static SoundManager GetInstance()
+    {
+        return instance;
+    }
 
     private void Awake()
     {
@@ -47,16 +52,11 @@ public class SoundManager : MonoBehaviour
         {
             foreach (Sound s in currentSounds)
             {
-                if (s.source != null)
+                if (s.source != null && !s.source.isPlaying)
                 {
-                    // Update the volume of each currently playing sound effect
-                    s.source.volume = volumeOfAllSounds * s.volume;
-                    if (!s.source.isPlaying)
-                    {
-                        // If the audio source is no longer playing, 
-                        //   remove it from the currently playing sounds list
-                        currentSounds.Remove(s);
-                    }
+                    // If the audio source is no longer playing, 
+                    //   remove it from the currently playing sounds list
+                    currentSounds.Remove(s);
                 }
             }
         }
@@ -112,6 +112,23 @@ public class SoundManager : MonoBehaviour
         else
         {
             Debug.LogWarning($"Sound \"{name}\" not found!");
+        }
+    }
+
+    // Takes a float from 0 to 1 and sets it to be the volume
+    public void SetVolume(float newVolume)
+    {
+        volumeOfAllSounds = (newVolume > 1) ? 1 : (newVolume == 0) ? 0 : Mathf.Log10(newVolume) * 20;
+        if (sounds != null && currentSounds != null)
+        {
+            foreach (Sound s in currentSounds)
+            {
+                if (s.source != null)
+                {
+                    // Update the volume of each currently playing sound effect
+                    s.source.volume = volumeOfAllSounds;
+                }
+            }
         }
     }
 }
