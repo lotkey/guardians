30f14f95ee4b6c882bd92296738d15a537d7151a
Ryan Rapier <rdrapier@live.com>
2021-04-01 01:42:15 -0700

    created place to put my Stress test.
    IF I HAD ONE  :(

--- Assets/tst/ryan/PlayMode_RR/EnemyTests.cs
+++ Assets/tst/ryan/PlayMode_RR/EnemyTests.cs
@@ -14,11 +14,20 @@ public class EnemyTests
     {
         // Use the Assert class to test conditions.
         // Use yield to skip a frame.
+        
         yield return null;
     }
 
     [UnityTest]
     public IEnumerator EnemyDealDamageTest()
+    {
+        // Use the Assert class to test conditions.
+        // Use yield to skip a frame.
+        yield return null;
+    }
+
+    [UnityTest]
+    public IEnumerator EnemyStressTest()
     {
         // Use the Assert class to test conditions.
         // Use yield to skip a frame.
