79f292c64c3116e7a9d541bcc49f0b595463df2c
Garrett <deepg2w@gmail.com>
2021-04-01 09:36:59 -0700

    Slider stuff works better now

--- Assets/src/collaborative/inventory/InventoryElement.cs
+++ Assets/src/collaborative/inventory/InventoryElement.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public class InventoryElement : MouseOver
 {
+    // reference to whatever weapon is in this slot
 	public InventoryItem weapon 
     {
-
         get{ return weapon;}
 
-        set{ weapon = value;}
-
-    } // reference to whatever weapon is in this slot
+        set
+        { 
+            weapon = value;
+            // TODO: this should update the sprite on the game object so that the player can see the item in the inventory
+            //this.gameObject.GetComponent<Image>().sprite = weapon.icon;
+        }
+    } 
 
     public override void OnPointerEnter(PointerEventData pointerEventData)
     {
