a311287bd320d523223af8f90da33586b3274846
Cade Disselkoen <zosimus.this@gmail.com>
2021-04-01 10:30:41 -0700

    Bullets fixed 1a

--- Assets/src/collaborative/Entities and Combat/cade/Bullets.cs
+++ Assets/src/collaborative/Entities and Combat/cade/Bullets.cs
@@ -7,7 +7,6 @@ public class Bullets : MonoBehaviour
     public float damage;
     public float speed;
     public Vector2 direction;
-    public GameObject gmeObject;
     public Rigidbody2D body;
     public Player player;
     public bool collision = false;
@@ -16,16 +15,17 @@ public class Bullets : MonoBehaviour
 
     public Bullets(Vector2 direction, float speed, float damage)
     {
+        //Set speed, direction and damage
         this.speed = speed;
         this.direction = direction;
         this.damage = damage;
-        
-        gmeObject = new GameObject();
     }
 
     private void Start()
     {
-        body = gmeObject.AddComponent<Rigidbody2D>();
+        //Adds rigidbody to gameObject
+        body = gameObject.AddComponent<Rigidbody2D>();
+        //Gets player
         Player.GetPlayer();
         //Set velocity
         body.velocity = direction * speed;
