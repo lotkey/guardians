a8922a4684347d2559a3d3c798c3c9aa0df64a7e
Leafeon4T <77814772+Leafeon4T@users.noreply.github.com>
2021-04-15 10:22:06 -0700

    Rooms Wave 2
    The Number of Rooms have been doubled!

--- Assets/src/michael/CavernScript.cs
+++ Assets/src/michael/CavernScript.cs
@@ -5,34 +5,19 @@ using UnityEditor;
 
 public class CavernScript : MonoBehaviour
 {
-    public GameObject Collapse1, Collapse2, Collapse3, Fish, Gem, SmallRm, Tunnel1, Tunnel2, Tunnel3, Tunnel4, pt1, pt2, pt3, pt4;
+    public GameObject
+        //Collapse1, Collapse2, Collapse3, Fish, Gem, SmallRm, 
+        Tunnel1, Tunnel2, Tunnel3, Tunnel4, Multi, pt1, pt2, pt3, pt4;
 
     // Start is called before the first frame update
     void Start()
     {
         int seed = Random.Range(0, 9999);
 
-        /*
-        Collapse1 = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/src/michael/Collapse1.prefab");
-        Collapse2 = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/src/michael/Collapse2.prefab");
-        Collapse3 = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/src/michael/Collapse3.prefab");
-        Fish = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/src/michael/Fish.prefab");
-        Gem = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/src/michael/Gem.prefab");
-        SmallRm = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/src/michael/SmallRm.prefab");
-        Tunnel1 = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/src/michael/Tunnel1.prefab");
-        Tunnel2 = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/src/michael/Tunnel2.prefab");
-        Tunnel3 = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/src/michael/Tunnel3.prefab");
-        Tunnel4 = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/src/michael/Tunnel4.prefab");
-        */
         roomSpawn(pt1, (seed % 10), 0);
         roomSpawn(pt2, (seed % 100) / 10, 1);
         roomSpawn(pt3, (seed % 1000) / 100, 2);
         roomSpawn(pt4, (seed) / 1000, 3);
-
-        //Instantiate(Collapse1, new Vector3(pt1.transform.position.x - 0.5f, pt1.transform.position.y + 9.5f, 0), Quaternion.Euler(0f, 0f, 90f));
-        //Instantiate(Collapse2, new Vector3(pt2.transform.position.x + 9.5f, pt2.transform.position.y + 0.5f, 0), Quaternion.Euler(0f, 0f, 0f));
-        //Instantiate(Collapse1, new Vector3(pt3.transform.position.x + 9.5f, pt3.transform.position.y + 0.5f, 0), Quaternion.Euler(0f, 0f, 0f));
-        //Instantiate(Collapse2, new Vector3(pt4.transform.position.x + 0.5f, pt4.transform.position.y - 9.5f, 0), Quaternion.Euler(0f, 0f, 270f));
     }
 
     // Update is called once per frame
@@ -46,35 +31,32 @@ public class CavernScript : MonoBehaviour
         if (end == 0)
             switch (seed)
             {
-                case 0:
-                    Instantiate(Collapse1, new Vector3(point.transform.position.x - 0.5f, point.transform.position.y + 9.5f, 0), Quaternion.Euler(0f, 0f, 90f));
-                    break;
                 case 1:
-                    Instantiate(Collapse2, new Vector3(point.transform.position.x - 0.5f, point.transform.position.y + 9.5f, 0), Quaternion.Euler(0f, 0f, 90f));
+                    Instantiate(Tunnel1, new Vector3(point.transform.position.x - 0.5f, point.transform.position.y + 9.5f, 0), Quaternion.Euler(0f, 0f, 90f));
                     break;
                 case 2:
-                    Instantiate(Collapse3, new Vector3(point.transform.position.x - 0.5f, point.transform.position.y + 9.5f, 0), Quaternion.Euler(0f, 0f, 90f));
+                    Instantiate(Tunnel1, new Vector3(point.transform.position.x - 0.5f, point.transform.position.y + 9.5f, 0), Quaternion.Euler(0f, 0f, 90f));
                     break;
                 case 3:
-                    Instantiate(Fish, new Vector3(point.transform.position.x - 0.5f, point.transform.position.y + 9.5f, 0), Quaternion.Euler(0f, 0f, 90f));
-                    break;
-                case 4:
-                    Instantiate(Gem, new Vector3(point.transform.position.x - 0.5f, point.transform.position.y + 9.5f, 0), Quaternion.Euler(0f, 0f, 90f));
-                    break;
-                case 5:
-                    Instantiate(SmallRm, new Vector3(point.transform.position.x - 0.5f, point.transform.position.y + 9.5f, 0), Quaternion.Euler(0f, 0f, 90f));
-                    break;
-                case 6:
-                    Object.Instantiate(Tunnel1, new Vector3(point.transform.position.x - 0.5f, point.transform.position.y + 9.5f, 0), Quaternion.Euler(0f, 0f, 90f));
-                    break;
-                case 7:
                     Instantiate(Tunnel2, new Vector3(point.transform.position.x - 0.5f, point.transform.position.y + 9.5f, 0), Quaternion.Euler(0f, 0f, 90f));
                     break;
-                case 8:
+                case 4:
+                    Instantiate(Tunnel2, new Vector3(point.transform.position.x - 0.5f, point.transform.position.y + 9.5f, 0), Quaternion.Euler(0f, 0f, 90f));
+                    break;
+                case 5:
                     Instantiate(Tunnel3, new Vector3(point.transform.position.x - 0.5f, point.transform.position.y + 9.5f, 0), Quaternion.Euler(0f, 0f, 90f));
                     break;
-                case 9:
+                case 6:
+                    Instantiate(Tunnel3, new Vector3(point.transform.position.x - 0.5f, point.transform.position.y + 9.5f, 0), Quaternion.Euler(0f, 0f, 90f));
+                    break;
+                case 7:
                     Instantiate(Tunnel4, new Vector3(point.transform.position.x - 0.5f, point.transform.position.y + 9.5f, 0), Quaternion.Euler(0f, 0f, 90f));
+                    break;
+                case 8:
+                    Instantiate(Tunnel4, new Vector3(point.transform.position.x - 0.5f, point.transform.position.y + 9.5f, 0), Quaternion.Euler(0f, 0f, 90f));
+                    break;
+                case 9:
+                    Instantiate(Multi, new Vector3(point.transform.position.x - 0.5f, point.transform.position.y + 9.5f, 0), Quaternion.Euler(0f, 0f, 90f));
                     break;
                 default:
                     Debug.Log($"Invalid Room Number Tried to Spawn: {seed}");
@@ -83,35 +65,32 @@ public class CavernScript : MonoBehaviour
         else if (end == 1)
             switch (seed)
             {
-                case 0:
-                    Instantiate(Collapse1, new Vector3(point.transform.position.x + 9.5f, point.transform.position.y + 0.5f, 0), Quaternion.Euler(0f, 0f, 0f));
-                    break;
                 case 1:
-                    Instantiate(Collapse2, new Vector3(point.transform.position.x + 9.5f, point.transform.position.y + 0.5f, 0), Quaternion.Euler(0f, 0f, 0f));
-                    break;
-                case 2:
-                    Instantiate(Collapse3, new Vector3(point.transform.position.x + 9.5f, point.transform.position.y + 0.5f, 0), Quaternion.Euler(0f, 0f, 0f));
-                    break;
-                case 3:
-                    Instantiate(Fish, new Vector3(point.transform.position.x + 9.5f, point.transform.position.y + 0.5f, 0), Quaternion.Euler(0f, 0f, 0f));
-                    break;
-                case 4:
-                    Instantiate(Gem, new Vector3(point.transform.position.x + 9.5f, point.transform.position.y + 0.5f, 0), Quaternion.Euler(0f, 0f, 0f));
-                    break;
-                case 5:
-                    Instantiate(SmallRm, new Vector3(point.transform.position.x + 9.5f, point.transform.position.y + 0.5f, 0), Quaternion.Euler(0f, 0f, 0f));
-                    break;
-                case 6:
                     Instantiate(Tunnel1, new Vector3(point.transform.position.x + 9.5f, point.transform.position.y + 0.5f, 0), Quaternion.Euler(0f, 0f, 0f));
                     break;
-                case 7:
+                case 2:
+                    Instantiate(Tunnel1, new Vector3(point.transform.position.x + 9.5f, point.transform.position.y + 0.5f, 0), Quaternion.Euler(0f, 0f, 0f));
+                    break;
+                case 3:
                     Instantiate(Tunnel2, new Vector3(point.transform.position.x + 9.5f, point.transform.position.y + 0.5f, 0), Quaternion.Euler(0f, 0f, 0f));
                     break;
-                case 8:
+                case 4:
+                    Instantiate(Tunnel2, new Vector3(point.transform.position.x + 9.5f, point.transform.position.y + 0.5f, 0), Quaternion.Euler(0f, 0f, 0f));
+                    break;
+                case 5:
                     Instantiate(Tunnel3, new Vector3(point.transform.position.x + 9.5f, point.transform.position.y + 0.5f, 0), Quaternion.Euler(0f, 0f, 0f));
                     break;
-                case 9:
+                case 6:
+                    Instantiate(Tunnel3, new Vector3(point.transform.position.x + 9.5f, point.transform.position.y + 0.5f, 0), Quaternion.Euler(0f, 0f, 0f));
+                    break;
+                case 7:
                     Instantiate(Tunnel4, new Vector3(point.transform.position.x + 9.5f, point.transform.position.y + 0.5f, 0), Quaternion.Euler(0f, 0f, 0f));
+                    break;
+                case 8:
+                    Instantiate(Tunnel4, new Vector3(point.transform.position.x + 9.5f, point.transform.position.y + 0.5f, 0), Quaternion.Euler(0f, 0f, 0f));
+                    break;
+                case 9:
+                    Instantiate(Multi, new Vector3(point.transform.position.x + 9.5f, point.transform.position.y + 0.5f, 0), Quaternion.Euler(0f, 0f, 0f));
                     break;
                 default:
                     Debug.Log($"Invalid Room Number Tried to Spawn: {seed}");
@@ -120,35 +99,32 @@ public class CavernScript : MonoBehaviour
         else if (end == 2)
             switch (seed)
             {
-                case 0:
-                    Instantiate(Collapse1, new Vector3(point.transform.position.x + 9.5f, point.transform.position.y + 0.5f, 0), Quaternion.Euler(0f, 0f, 0f));
-                    break;
                 case 1:
-                    Instantiate(Collapse2, new Vector3(point.transform.position.x + 9.5f, point.transform.position.y + 0.5f, 0), Quaternion.Euler(0f, 0f, 0f));
-                    break;
-                case 2:
-                    Instantiate(Collapse3, new Vector3(point.transform.position.x + 9.5f, point.transform.position.y + 0.5f, 0), Quaternion.Euler(0f, 0f, 0f));
-                    break;
-                case 3:
-                    Instantiate(Fish, new Vector3(point.transform.position.x + 9.5f, point.transform.position.y + 0.5f, 0), Quaternion.Euler(0f, 0f, 0f));
-                    break;
-                case 4:
-                    Instantiate(Gem, new Vector3(point.transform.position.x + 9.5f, point.transform.position.y + 0.5f, 0), Quaternion.Euler(0f, 0f, 0f));
-                    break;
-                case 5:
-                    Instantiate(SmallRm, new Vector3(point.transform.position.x + 9.5f, point.transform.position.y + 0.5f, 0), Quaternion.Euler(0f, 0f, 0f));
-                    break;
-                case 6:
                     Instantiate(Tunnel1, new Vector3(point.transform.position.x + 9.5f, point.transform.position.y + 0.5f, 0), Quaternion.Euler(0f, 0f, 0f));
                     break;
-                case 7:
+                case 2:
+                    Instantiate(Tunnel1, new Vector3(point.transform.position.x + 9.5f, point.transform.position.y + 0.5f, 0), Quaternion.Euler(0f, 0f, 0f));
+                    break;
+                case 3:
                     Instantiate(Tunnel2, new Vector3(point.transform.position.x + 9.5f, point.transform.position.y + 0.5f, 0), Quaternion.Euler(0f, 0f, 0f));
                     break;
-                case 8:
+                case 4:
+                    Instantiate(Tunnel2, new Vector3(point.transform.position.x + 9.5f, point.transform.position.y + 0.5f, 0), Quaternion.Euler(0f, 0f, 0f));
+                    break;
+                case 5:
                     Instantiate(Tunnel3, new Vector3(point.transform.position.x + 9.5f, point.transform.position.y + 0.5f, 0), Quaternion.Euler(0f, 0f, 0f));
                     break;
-                case 9:
+                case 6:
+                    Instantiate(Tunnel3, new Vector3(point.transform.position.x + 9.5f, point.transform.position.y + 0.5f, 0), Quaternion.Euler(0f, 0f, 0f));
+                    break;
+                case 7:
                     Instantiate(Tunnel4, new Vector3(point.transform.position.x + 9.5f, point.transform.position.y + 0.5f, 0), Quaternion.Euler(0f, 0f, 0f));
+                    break;
+                case 8:
+                    Instantiate(Tunnel4, new Vector3(point.transform.position.x + 9.5f, point.transform.position.y + 0.5f, 0), Quaternion.Euler(0f, 0f, 0f));
+                    break;
+                case 9:
+                    Instantiate(Multi, new Vector3(point.transform.position.x + 9.5f, point.transform.position.y + 0.5f, 0), Quaternion.Euler(0f, 0f, 0f));
                     break;
                 default:
                     Debug.Log($"Invalid Room Number Tried to Spawn: {seed}");
@@ -157,35 +133,32 @@ public class CavernScript : MonoBehaviour
         else if (end == 3)
             switch (seed)
             {
-                case 0:
-                    Instantiate(Collapse1, new Vector3(point.transform.position.x + 0.5f, point.transform.position.y - 9.5f, 0), Quaternion.Euler(0f, 0f, 270f));
-                    break;
                 case 1:
-                    Instantiate(Collapse2, new Vector3(point.transform.position.x + 0.5f, point.transform.position.y - 9.5f, 0), Quaternion.Euler(0f, 0f, 270f));
-                    break;
-                case 2:
-                    Instantiate(Collapse3, new Vector3(point.transform.position.x + 0.5f, point.transform.position.y - 9.5f, 0), Quaternion.Euler(0f, 0f, 270f));
-                    break;
-                case 3:
-                    Instantiate(Fish, new Vector3(point.transform.position.x + 0.5f, point.transform.position.y - 9.5f, 0), Quaternion.Euler(0f, 0f, 270f));
-                    break;
-                case 4:
-                    Instantiate(Gem, new Vector3(point.transform.position.x + 0.5f, point.transform.position.y - 9.5f, 0), Quaternion.Euler(0f, 0f, 270f));
-                    break;
-                case 5:
-                    Instantiate(SmallRm, new Vector3(point.transform.position.x + 0.5f, point.transform.position.y - 9.5f, 0), Quaternion.Euler(0f, 0f, 270f));
-                    break;
-                case 6:
                     Instantiate(Tunnel1, new Vector3(point.transform.position.x + 0.5f, point.transform.position.y - 9.5f, 0), Quaternion.Euler(0f, 0f, 270f));
                     break;
-                case 7:
+                case 2:
+                    Instantiate(Tunnel1, new Vector3(point.transform.position.x + 0.5f, point.transform.position.y - 9.5f, 0), Quaternion.Euler(0f, 0f, 270f));
+                    break;
+                case 3:
                     Instantiate(Tunnel2, new Vector3(point.transform.position.x + 0.5f, point.transform.position.y - 9.5f, 0), Quaternion.Euler(0f, 0f, 270f));
                     break;
-                case 8:
+                case 4:
+                    Instantiate(Tunnel2, new Vector3(point.transform.position.x + 0.5f, point.transform.position.y - 9.5f, 0), Quaternion.Euler(0f, 0f, 270f));
+                    break;
+                case 5:
                     Instantiate(Tunnel3, new Vector3(point.transform.position.x + 0.5f, point.transform.position.y - 9.5f, 0), Quaternion.Euler(0f, 0f, 270f));
                     break;
-                case 9:
+                case 6:
+                    Instantiate(Tunnel3, new Vector3(point.transform.position.x + 0.5f, point.transform.position.y - 9.5f, 0), Quaternion.Euler(0f, 0f, 270f));
+                    break;
+                case 7:
                     Instantiate(Tunnel4, new Vector3(point.transform.position.x + 0.5f, point.transform.position.y - 9.5f, 0), Quaternion.Euler(0f, 0f, 270f));
+                    break;
+                case 8:
+                    Instantiate(Tunnel4, new Vector3(point.transform.position.x + 0.5f, point.transform.position.y - 9.5f, 0), Quaternion.Euler(0f, 0f, 270f));
+                    break;
+                case 9:
+                    Instantiate(Multi, new Vector3(point.transform.position.x + 0.5f, point.transform.position.y - 9.5f, 0), Quaternion.Euler(0f, 0f, 270f));
                     break;
                 default:
                     Debug.Log($"Invalid Room Number Tried to Spawn: {seed}");
