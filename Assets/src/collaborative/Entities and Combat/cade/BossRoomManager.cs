64a28fd8b0a57d3ce0c17e6436996b402c86c591
Ryan Rapier <rdrapier@live.com>
2021-05-05 19:01:11 -0700

    updated BossRoomManager.cs
    so it doesn't update HUDManager every update function call.

--- Assets/src/collaborative/Entities and Combat/cade/BossRoomManager.cs
+++ Assets/src/collaborative/Entities and Combat/cade/BossRoomManager.cs
@@ -6,11 +6,17 @@ public class BossRoomManager : MonoBehaviour
 {
     public GameObject Boss;
 
+    private int haventWon = 1;
+
     void Update()
     {
-        if(Time.time > 10) {
-        	// TODO: fix this so it doesn't call literally every update
-           if(Boss == null) HUDManager.Instance.WonGame();
+        if( (haventWon == 1) && (Time.time > 10) ) 
+        {
+            if(Boss == null) 
+            {
+                HUDManager.Instance.WonGame();
+                haventWon = 0; // hopefully won't call every update now.
+            }            
         }
     }
 }
